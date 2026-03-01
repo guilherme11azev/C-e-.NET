4b145b57050217cd2edff17c3bebd9d16d19a48b
guilherme11azev <guilherme.azevti@gmail.com>
2026-03-01 00:45:09 -0300

    Add files via upload
    Projeto locadora.

---
+++ Locadora/Locadora/Carro.cs
@@ -0,0 +1,27 @@
+﻿class Carro
+{
+    public string Modelo { get; set; }
+    public string Marca { get; set; }
+    public int Ano { get; set; }
+    public int Preco { get; set; }
+    public bool disponivel { get; set; }
+    public string DescricaoResumida =>
+        $"O véiculo {Modelo} pertence à marca {Marca}, é do ano {Ano} e custa R${Preco} por dia.";
+    
+    public void ExibirFicha()
+    {
+        Console.WriteLine($"Modelo: {Modelo}");
+        Console.WriteLine($"Marca: {Marca}");
+        Console.WriteLine($"Ano: {Ano}");
+        Console.WriteLine($"Preco: {Preco}");
+        if (disponivel)
+        {
+            Console.WriteLine("Veículo disponível para locação.");
+        }
+        else
+        {
+            Console.WriteLine("Veículo indisponível para locação.");
+        }
+    }
+
+}
---
+++ Locadora/Locadora/Program.cs
@@ -0,0 +1,31 @@
+﻿Carro carro1 = new Carro();
+carro1.Modelo = "Civic";
+carro1.Marca = "Honda";
+carro1.Ano = 2020;
+carro1.Preco = 150;
+carro1.disponivel = true;
+Console.WriteLine(carro1.DescricaoResumida);
+
+Carro carro2 = new Carro();
+carro2.Modelo = "Corolla";
+carro2.Marca = "Toyota";
+carro2.Ano = 2019;
+carro2.Preco = 130;
+carro2.disponivel = false;
+Console.WriteLine(carro2.DescricaoResumida);
+
+Carro carro3 = new Carro();
+carro3.Modelo = "Fiesta";
+carro3.Marca = "Ford";
+carro3.Ano = 2018;
+carro3.Preco =100;
+carro3.disponivel = true;
+Console.WriteLine(carro3.DescricaoResumida);
+
+Carro carro4 = new Carro();
+carro4.Modelo = "Onix";
+carro4.Marca = "Chevrolet";
+carro4.Ano = 2021;
+carro4.Preco = 120;
+carro4.disponivel = true;
+Console.WriteLine(carro4.DescricaoResumida);
