efe431b5fbd391d27ffb34e8ad34fe0c56747f07
guilherme11azev <guilherme.azevti@gmail.com>
2026-03-01 20:08:26 -0300

    Upload arquivos estudos

---
+++ ScreenSound/ScreenSound/Album.cs
@@ -0,0 +1,12 @@
+﻿class Album
+{
+    private List<Musica> musicas;
+    public string Nome { get; set; }
+    public int DuracaoTotal { get; set; }
+
+
+    public void AdicioarMusica(Musica musica)
+    {
+
+    }
+}
