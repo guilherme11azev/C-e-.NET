4f7db3fe374ec166d4aff2481b2e8a69cb9a658e
guilherme11azev <guilherme.azevti@gmail.com>
2026-03-03 23:05:30 -0300

    Refactor music handling to use Banda and Album classes

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,15 +1,23 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.DescricaoResumida);
+Banda queen = new Banda("Queen");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Vertigo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = false;
+Album albumDoQueen = new Album("A night at the opera");
+
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+albumDoQueen.AdicioarMusica(musica1);
+albumDoQueen.AdicioarMusica(musica2);
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
